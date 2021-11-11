cc9580bd5667d84427f674ebf0a57982eebe5b64
antoniocruzrivera0216@gmail.com <antoniocruzrivera0216@gmail.com>
2021-11-10 19:54:38 -0600

    Agregar archivos de proyecto.

---
+++ Semaforo/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Semaforo
+{
+    partial class frmSemaforo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlVerticalRojo1 = new System.Windows.Forms.Panel();
+            this.pnlVerticalVerde1 = new System.Windows.Forms.Panel();
+            this.pnlVerticalAmarillo1 = new System.Windows.Forms.Panel();
+            this.pnlVerticalAmarillo2 = new System.Windows.Forms.Panel();
+            this.pnlVerticalRojo2 = new System.Windows.Forms.Panel();
+            this.pnlVerticalVerde2 = new System.Windows.Forms.Panel();
+            this.pnlHorizontalAmarillo1 = new System.Windows.Forms.Panel();
+            this.pnlHorizontalRojo1 = new System.Windows.Forms.Panel();
+            this.pnlVerdeHorizontal1 = new System.Windows.Forms.Panel();
+            this.pnlHorizontalAmarillo2 = new System.Windows.Forms.Panel();
+            this.pnlHorizontalVerde2 = new System.Windows.Forms.Panel();
+            this.pnlHorizontalRojo2 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.tmrVerde = new System.Windows.Forms.Timer(this.components);
+            this.tmrVerdeParpadeando = new System.Windows.Forms.Timer(this.components);
+            this.tmrAmarillo = new System.Windows.Forms.Timer(this.components);
+            this.tmrAmarilloParpadeando = new System.Windows.Forms.Timer(this.components);
+            this.tmrRojo = new System.Windows.Forms.Timer(this.components);
+            this.txtContador = new System.Windows.Forms.Label();
+            this.panel13.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlVerticalRojo1
+            // 
+            this.pnlVerticalRojo1.Location = new System.Drawing.Point(311, 28);
+            this.pnlVerticalRojo1.Name = "pnlVerticalRojo1";
+            this.pnlVerticalRojo1.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerticalRojo1.TabIndex = 0;
+            // 
+            // pnlVerticalVerde1
+            // 
+            this.pnlVerticalVerde1.Location = new System.Drawing.Point(311, 93);
+            this.pnlVerticalVerde1.Name = "pnlVerticalVerde1";
+            this.pnlVerticalVerde1.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerticalVerde1.TabIndex = 1;
+            // 
+            // pnlVerticalAmarillo1
+            // 
+            this.pnlVerticalAmarillo1.Location = new System.Drawing.Point(311, 61);
+            this.pnlVerticalAmarillo1.Name = "pnlVerticalAmarillo1";
+            this.pnlVerticalAmarillo1.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerticalAmarillo1.TabIndex = 2;
+            // 
+            // pnlVerticalAmarillo2
+            // 
+            this.pnlVerticalAmarillo2.Location = new System.Drawing.Point(246, 272);
+            this.pnlVerticalAmarillo2.Name = "pnlVerticalAmarillo2";
+            this.pnlVerticalAmarillo2.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerticalAmarillo2.TabIndex = 5;
+            // 
+            // pnlVerticalRojo2
+            // 
+            this.pnlVerticalRojo2.Location = new System.Drawing.Point(246, 304);
+            this.pnlVerticalRojo2.Name = "pnlVerticalRojo2";
+            this.pnlVerticalRojo2.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerticalRojo2.TabIndex = 4;
+            // 
+            // pnlVerticalVerde2
+            // 
+            this.pnlVerticalVerde2.Location = new System.Drawing.Point(246, 239);
+            this.pnlVerticalVerde2.Name = "pnlVerticalVerde2";
+            this.pnlVerticalVerde2.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerticalVerde2.TabIndex = 3;
+            // 
+            // pnlHorizontalAmarillo1
+            // 
+            this.pnlHorizontalAmarillo1.Location = new System.Drawing.Point(170, 135);
+            this.pnlHorizontalAmarillo1.Name = "pnlHorizontalAmarillo1";
+            this.pnlHorizontalAmarillo1.Size = new System.Drawing.Size(36, 36);
+            this.pnlHorizontalAmarillo1.TabIndex = 8;
+            // 
+            // pnlHorizontalRojo1
+            // 
+            this.pnlHorizontalRojo1.Location = new System.Drawing.Point(140, 135);
+            this.pnlHorizontalRojo1.Name = "pnlHorizontalRojo1";
+            this.pnlHorizontalRojo1.Size = new System.Drawing.Size(36, 36);
+            this.pnlHorizontalRojo1.TabIndex = 7;
+            // 
+            // pnlVerdeHorizontal1
+            // 
+            this.pnlVerdeHorizontal1.Location = new System.Drawing.Point(202, 135);
+            this.pnlVerdeHorizontal1.Name = "pnlVerdeHorizontal1";
+            this.pnlVerdeHorizontal1.Size = new System.Drawing.Size(36, 36);
+            this.pnlVerdeHorizontal1.TabIndex = 6;
+            // 
+            // pnlHorizontalAmarillo2
+            // 
+            this.pnlHorizontalAmarillo2.Location = new System.Drawing.Point(389, 189);
+            this.pnlHorizontalAmarillo2.Name = "pnlHorizontalAmarillo2";
+            this.pnlHorizontalAmarillo2.Size = new System.Drawing.Size(36, 36);
+            this.pnlHorizontalAmarillo2.TabIndex = 11;
+            // 
+            // pnlHorizontalVerde2
+            // 
+            this.pnlHorizontalVerde2.Location = new System.Drawing.Point(359, 189);
+            this.pnlHorizontalVerde2.Name = "pnlHorizontalVerde2";
+            this.pnlHorizontalVerde2.Size = new System.Drawing.Size(36, 36);
+            this.pnlHorizontalVerde2.TabIndex = 10;
+            // 
+            // pnlHorizontalRojo2
+            // 
+            this.pnlHorizontalRojo2.Location = new System.Drawing.Point(421, 189);
+            this.pnlHorizontalRojo2.Name = "pnlHorizontalRojo2";
+            this.pnlHorizontalRojo2.Size = new System.Drawing.Size(36, 36);
+            this.pnlHorizontalRojo2.TabIndex = 9;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel13.Controls.Add(this.txtContador);
+            this.panel13.Location = new System.Drawing.Point(244, 4);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(103, 412);
+            this.panel13.TabIndex = 12;
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel14.Location = new System.Drawing.Point(6, 135);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(608, 90);
+            this.panel14.TabIndex = 13;
+            // 
+            // tmrVerde
+            // 
+            this.tmrVerde.Interval = 1000;
+            this.tmrVerde.Tick += new System.EventHandler(this.tmrVerde_Tick);
+            // 
+            // tmrVerdeParpadeando
+            // 
+            this.tmrVerdeParpadeando.Interval = 500;
+            this.tmrVerdeParpadeando.Tick += new System.EventHandler(this.tmrVerdeParpadeando_Tick);
+            // 
+            // tmrAmarillo
+            // 
+            this.tmrAmarillo.Interval = 1000;
+            this.tmrAmarillo.Tick += new System.EventHandler(this.tmrAmarillo_Tick);
+            // 
+            // tmrRojo
+            // 
+            this.tmrRojo.Interval = 1000;
+            this.tmrRojo.Tick += new System.EventHandler(this.tmrRojo_Tick);
+            // 
+            // txtContador
+            // 
+            this.txtContador.AutoSize = true;
+            this.txtContador.Location = new System.Drawing.Point(29, 169);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.Size = new System.Drawing.Size(13, 13);
+            this.txtContador.TabIndex = 0;
+            this.txtContador.Text = "0";
+            // 
+            // frmSemaforo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 418);
+            this.Controls.Add(this.pnlHorizontalAmarillo2);
+            this.Controls.Add(this.pnlHorizontalVerde2);
+            this.Controls.Add(this.pnlHorizontalRojo2);
+            this.Controls.Add(this.pnlHorizontalAmarillo1);
+            this.Controls.Add(this.pnlHorizontalRojo1);
+            this.Controls.Add(this.pnlVerdeHorizontal1);
+            this.Controls.Add(this.pnlVerticalAmarillo2);
+            this.Controls.Add(this.pnlVerticalRojo2);
+            this.Controls.Add(this.pnlVerticalVerde2);
+            this.Controls.Add(this.pnlVerticalAmarillo1);
+            this.Controls.Add(this.pnlVerticalVerde1);
+            this.Controls.Add(this.pnlVerticalRojo1);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel14);
+            this.Name = "frmSemaforo";
+            this.Text = "Semaforo";
+            this.Load += new System.EventHandler(this.frmSemaforo_Load);
+            this.panel13.ResumeLayout(false);
+            this.panel13.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlVerticalRojo1;
+        private System.Windows.Forms.Panel pnlVerticalVerde1;
+        private System.Windows.Forms.Panel pnlVerticalAmarillo1;
+        private System.Windows.Forms.Panel pnlVerticalAmarillo2;
+        private System.Windows.Forms.Panel pnlVerticalRojo2;
+        private System.Windows.Forms.Panel pnlVerticalVerde2;
+        private System.Windows.Forms.Panel pnlHorizontalAmarillo1;
+        private System.Windows.Forms.Panel pnlHorizontalRojo1;
+        private System.Windows.Forms.Panel pnlVerdeHorizontal1;
+        private System.Windows.Forms.Panel pnlHorizontalAmarillo2;
+        private System.Windows.Forms.Panel pnlHorizontalVerde2;
+        private System.Windows.Forms.Panel pnlHorizontalRojo2;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Timer tmrVerde;
+        private System.Windows.Forms.Timer tmrVerdeParpadeando;
+        private System.Windows.Forms.Timer tmrAmarillo;
+        private System.Windows.Forms.Timer tmrAmarilloParpadeando;
+        private System.Windows.Forms.Timer tmrRojo;
+        private System.Windows.Forms.Label txtContador;
+    }
+}
+
---
+++ Semaforo/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaforo
+{
+    public partial class frmSemaforo : Form
+    {
+        double segundos = 0;
+        public frmSemaforo()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrVerde_Tick(object sender, EventArgs e)
+        {
+            if (segundos == 10)
+            {
+                segundos = 0;
+                tmrVerde.Stop();
+                tmrVerdeParpadeando.Start();
+                pnlVerticalVerde1.BackColor = Color.Gray;
+                pnlVerticalVerde2.BackColor = Color.Gray;
+            }
+            else
+            {
+                segundos++;
+                txtContador.Text = segundos.ToString();
+
+            }
+        }
+
+        private void frmSemaforo_Load(object sender, EventArgs e)
+        {
+            tmrVerde.Start();
+            pnlVerticalVerde1.BackColor = Color.Green;
+            pnlVerticalVerde2.BackColor = Color.Green;
+        }
+
+        private void tmrVerdeParpadeando_Tick(object sender, EventArgs e)
+        {
+            if (segundos == 3)
+            {
+                segundos = 0;
+                tmrVerdeParpadeando.Stop();
+                tmrAmarillo.Start();
+                pnlVerticalVerde1.BackColor = Color.Gray;
+                pnlVerticalVerde2.BackColor = Color.Gray;
+            }
+            else
+            {
+                if (!(Math.Abs(segundos % 1) <= (Double.Epsilon * 100)))
+                {
+                    pnlVerticalVerde1.BackColor = Color.Green;
+                    pnlVerticalVerde2.BackColor = Color.Green;
+                }
+                else
+                {
+                    pnlVerticalVerde1.BackColor = Color.Gray;
+                    pnlVerticalVerde2.BackColor = Color.Gray;
+                }
+                segundos++;
+                txtContador.Text = segundos.ToString();
+            }
+        }
+
+        private void tmrAmarillo_Tick(object sender, EventArgs e)
+        {
+            pnlVerticalAmarillo1.BackColor = Color.Yellow;
+            pnlVerticalAmarillo2.BackColor = Color.Yellow;
+            if (segundos == 3)
+            {
+                segundos = 0;
+                tmrAmarillo.Stop();
+                tmrRojo.Start();
+                pnlVerticalAmarillo1.BackColor = Color.Gray;
+                pnlVerticalAmarillo2.BackColor = Color.Gray;
+            }
+            else
+            {
+                segundos++;
+                txtContador.Text = segundos.ToString();
+
+            }
+        }
+
+        private void tmrRojo_Tick(object sender, EventArgs e)
+        {
+            pnlVerticalRojo1.BackColor = Color.Red;
+            pnlVerticalRojo2.BackColor = Color.Red;
+            if (segundos == 3)
+            {
+                segundos = 0;
+                tmrRojo.Stop();
+                tmrVerde.Start();
+                pnlVerticalRojo1.BackColor = Color.Gray;
+                pnlVerticalRojo2.BackColor = Color.Gray;
+            }
+            else
+            {
+                segundos++;
+                txtContador.Text = segundos.ToString();
+
+            }
+        }
+    }
+}
